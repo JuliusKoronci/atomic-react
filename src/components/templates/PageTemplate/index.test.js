046ca63c34f17c8d16d241d08515625a8b076c22
Julius Koronci <koronci@trimbroker.com>
2017-02-23 21:26:38 +0100

    Initial commit

---
+++ src/components/templates/PageTemplate/index.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import PageTemplate from './index';
+
+it('mounts', () => {
+	mount(<PageTemplate>test</PageTemplate>);
+});
---
+++ src/store/actions.js
@@ -0,0 +1,10 @@
+import forIn from 'lodash/forIn';
+
+const req = require.context('.', true, /\.\/.+\/actions\.js$/);
+
+req.keys().forEach((key) => {
+	const actions = req(key);
+	forIn(actions, (action, name) => {
+		module.exports[name] = action;
+	});
+});
